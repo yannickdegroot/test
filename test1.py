bc7f4d95afc2a7f163e480751bb2084569531fe9
yannickdegroot <yjcdegroot@hotmail.com>
2023-08-31 11:05:01 +0200

    Added 3 new files

---
+++ test1.py
@@ -0,0 +1,6 @@
+amount = int(input("amount"))
+price = float(input("price"))
+total_price = (amount * price)
+print("amount:", amount)
+print("price:", price, "euro")
+print("total price:", total_price, "euro")
