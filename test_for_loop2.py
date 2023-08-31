3cc59b8dd3a9d4989c015f2b86dc93818af6b7d2
yannickdegroot <yjcdegroot@hotmail.com>
2023-08-31 11:35:16 +0200

    Adding 2 new files

---
+++ test_for_loop2.py
@@ -0,0 +1,8 @@
+import time
+
+for a in range (1,3):
+    print("enkel:", a)
+    print("dubbel:", a*2)
+    time.sleep(2)
+
+print("En we zijn weer uit de loop")
---
+++ test_for_loop3.py
@@ -0,0 +1,5 @@
+import time
+display = "YANNICK DE GROOT"
+for i in display:
+    print(i)
+    time.sleep(1)
