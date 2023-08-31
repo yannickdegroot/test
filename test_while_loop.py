35e6c56f4c0a65200ee433a109067c07a68031bd
yannickdegroot <yjcdegroot@hotmail.com>
2023-08-31 15:56:33 +0200

    Added 1 file

---
+++ test_while_loop.py
@@ -0,0 +1,16 @@
+#Comment to the below code. This comment will be ignored by the Interpreter.
+import time
+
+i = 0
+
+''''
+Comment that will be ignored by the interpreter
+'''
+
+while i <6:
+    print(i)
+    i = i + 1
+    time.sleep(1)
+
+#Comment to the below code. This comment will be ignored by the Interpreter.
+print("eind")
