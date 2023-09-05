654e13968247d03eefaea42a01f6f974f08b0d96
yannickdegroot <yjcdegroot@hotmail.com>
2023-09-05 17:48:26 +0200

    adding 2 files

---
+++ test_dictionary.py
@@ -0,0 +1,10 @@
+mijn_dictionary = {
+    "product" : "softijs",
+    "aantal" : 101,
+    "smaak" : "vanille"
+}
+
+keys = mijn_dictionary.keys()
+values = mijn_dictionary.values()
+print("keys", keys)
+print("values", values)
---
+++ test_dictionary2.py
@@ -0,0 +1,8 @@
+mijn_dictionary = {
+    "product" : "softijs",
+    "aantal" : 101,
+    "smaak" : "vanille"
+}
+
+for k, v in mijn_dictionary.items():
+    print(k, v)
